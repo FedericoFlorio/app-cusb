c3386ba48fc51ae93928887dc2cda91112c28f45
coro.uni.sb18@gmail.com <coro.uni.sb18@gmail.com>
2023-09-23 17:30:28 +0000

    Created main and editor; Edited settings; Deleted Form1

---
+++ client_code/main/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import mainTemplate
+from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+
+class main(mainTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_login_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.login_with_form()
+    pass
+
