f044617f19044a09918c3bdb9a545f0c2de2c268
coro.uni.sb18@gmail.com <coro.uni.sb18@gmail.com>
2023-09-28 14:43:17 +0000

    Edited editor.ItemTemplate4

--- client_code/editor/ItemTemplate4/__init__.py
+++ client_code/editor/ItemTemplate4/__init__.py
@@ -14,3 +14,9 @@ class ItemTemplate4(ItemTemplate4Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
+    pass
+
