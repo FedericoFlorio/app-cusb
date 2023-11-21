b64a08f67925ffc5e3b6424ec7bde90505c1d427
coro.uni.sb18@gmail.com <coro.uni.sb18@gmail.com>
2023-11-21 09:52:13 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -38,14 +38,20 @@ def check_format(s):
   err = 0
   title = s[0]
   intro = s[1]
-  beg = s[2]
-  end = s[len(s)-1]
-  body = s[3:len(s)-1]
   if title.split()[0] != "\head":
     err = 1
-  if intro.split()[0] != "\intro":
-    if 
-    err = 2
+  if intro.split()[0] == "\intro":
+    end = s[len(s)-1]
+    body = s[3:len(s)-1]
+    beg = s[2]
+  else:
+    if intro.split()[0] == "\start":
+      end = s[len(s)-1]
+      body = s[2:len(s)-1]
+      beg = s[1]
+    else:
+      err = 2
+
   if beg.split()[0] != "\start":
     print(beg.split()[0])
     err = 3
